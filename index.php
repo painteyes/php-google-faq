ce96c967ebc4881f87a4e3d2094cc1e69e29d453
painteyes <andreantoniocchipinti@gmail.com>
2022-01-25 18:14:16 +0100

    done

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ Creare il “Database” (struttura dati) e la visualizzazione di queste domande
 
 <body>
 
+
 <?php 
 
     $frequently_asked_questions = [
@@ -28,44 +29,44 @@ Creare il “Database” (struttura dati) e la visualizzazione di queste domande
 
             'answer' => "
 
-            La recente <a href='#'>decisione della Corte di giustizia dell\'Unione europea</a> ha profonde conseguenze per i motori di ricerca 
+            La recente <a href=''>decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca 
             in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google 
             di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, 
             i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi. 
             
-            <br><br>
+            <br><br><br>
 
             Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci 
             a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un 
-            bilanciamento tra il diritto dell\'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere 
+            bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere 
             e distribuire le informazioni. 
             
-            <br><br>
+            <br><br><br>
 
-            Per presentare una richiesta di rimozione, compila questo <a href='#'>modulo web</a>. Riceverai una risposta automatica che conferma 
-            la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po\' di 
+            Per presentare una richiesta di rimozione, compila questo <a href=''>modulo web</a>. Riceverai una risposta automatica che conferma 
+            la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di 
             tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati 
             includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico 
             a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, 
             negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio 
             (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere 
-            nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d\'accordo con la nostra valutazione, 
-            puoi rivolgerti all\'Autorità garante per la protezione dei dati personali nel tuo paese. 
+            nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, 
+            puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese. 
             
-            <br><br>
+            <br><br><br>
 
             Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità 
             per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori 
             di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione 
             della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge. 
             
-            <br><br>
+            <br><br><br>
             
             Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati 
             modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti 
             europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.
 
-            ";
+            "
         ],
 
         [
@@ -74,35 +75,35 @@ Creare il “Database” (struttura dati) e la visualizzazione di queste domande
             'answer' => "
 
             Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo 
-            offrire un\'elevata sicurezza e assicurarti che le tue informazioni 
+            offrire un'elevata sicurezza e assicurarti che le tue informazioni 
             sono al sicuro e accessibili quando ne hai bisogno. 
             
-            <br><br>
+            <br><br><br>
 
-            Siamo costantemente al lavoro per garantire un\'elevata sicurezza, proteggere la tua privacy e rendere Google 
+            Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google 
             ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza 
             e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. 
             Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, 
             la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con 
             Google, hai il massimo controllo. 
             
-            <br><br>
+            <br><br><br>
 
             Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, 
-            nel <a href='#'>Centro Google per la sicurezza online</a>. 
+            nel <a href=''>Centro Google per la sicurezza online</a>. 
             
-            <br><br>
+            <br><br><br>
 
-            <a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te 
+            <a href=''>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te 
             il controllo. 
             
-            <br><br>
+            <br><br><br>
             
             Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati 
             modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei 
             quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.
 
-            ";
+            "
         ],
 
         [
@@ -112,28 +113,32 @@ Creare il “Database” (struttura dati) e la visualizzazione di queste domande
 
             Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
             
-            <br><br>
+            <br><br><br>
 
             <ol>
                 <li>
-
-                    La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile 
-                    del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per 
-                    i consumatori tramite una delle due società seguenti:
-
+                    <span>
+                        La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile 
+                        del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per 
+                        i consumatori tramite una delle due società seguenti:
+                    </span>
+                    
                     <br><br>
 
                     <ol class='alphabetical-ol'> 
-
                         <li>
-                            Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo 
-                            (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+                            <span>
+                                Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo 
+                                (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+                            </span>
                         </li>
                         
                         <br>
                         
                         <li>
-                            Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+                            <span>
+                                Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+                            </span>
                         </li>
                     </ol>
                 </li>
@@ -141,7 +146,9 @@ Creare il “Database” (struttura dati) e la visualizzazione di queste domande
                 <br>
 
                 <li>
-                    La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+                    <span>
+                        La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+                    </span>
                 </li>
             </ol>
             
@@ -150,25 +157,25 @@ Creare il “Database” (struttura dati) e la visualizzazione di queste domande
             Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata 
             che li offre o dal paese a cui è associato il tuo account.
 
-            <h5>Stabilire il paese associato al tuo account</h5>
+            <h3>Stabilire il paese associato al tuo account</h3>
 
             Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. 
             Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi 
-            solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell\'ultimo anno.
+            solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
             
-            <br><br>
+            <br><br><br>
 
             I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci 
-            in un altro paese, potrebbe occorrere circa un anno per aggiornare l\'associazione del paese.
+            in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
 
-            <br><br>
+            <br><br><br>
 
             Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe 
-            essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l\'installazione di una 
+            essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una 
             rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine 
-            territoriale. <a href='#'>Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.
+            territoriale. <a href=''>Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.
 
-            ";
+            "
         ],
         
         [
@@ -204,7 +211,7 @@ Creare il “Database” (struttura dati) e la visualizzazione di queste domande
             o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' 
             esatte parole chiave che hanno determinato il clic su un annuncio.
             
-            ";
+            "
         ]
     ]
 ?>
